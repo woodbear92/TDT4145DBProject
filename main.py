3e961870716e6337bdef4683e4406f71da26eb6e
Vebjørn Steinsholt <woodbear92@gmail.com>
2021-03-18 09:52:44 +0100

    Update main.py
    When a user is created it is added to Instructor/student and PostCreator. All ids are now randomly generated.

--- main.py
+++ main.py
@@ -10,14 +10,34 @@ mydb = mysql.connector.connect(
 
 
 # Functional
-def create_user(userName, email):
+def create_user(userName, email, Instructor):
     mycursor = mydb.cursor()
-    generatedKey = uuid.uuid1()
+    generatedKey = uuid.uuid4()
     id = generatedKey.bytes
+    generatedKey = uuid.uuid4()
+    PCID = generatedKey.bytes
     sql = "INSERT INTO User(UserID,UserName, UserEmail) VALUES (%s, %s, %s)"
     val = (id, userName, email)
     mycursor.execute(sql, val)
     mydb.commit()
+    if(Instructor):
+        sql = "INSERT INTO PostCreator(PCID, CreatorType) VALUES (%s, %s)"
+        val = (PCID, "Instructor")
+        mycursor.execute(sql, val)
+        mydb.commit()
+        sql = "INSERT INTO Instructor(InstructorId,PCID) VALUES (%s, %s)"
+        val = (id, PCID)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    else:
+        sql = "INSERT INTO PostCreator(PCID, CreatorType) VALUES (%s, %s)"
+        val = (PCID, "Student")
+        mycursor.execute(sql, val)
+        mydb.commit()
+        sql = "INSERT INTO Student(StudentId,PCID) VALUES (%s, %s)"
+        val = (id, PCID)
+        mycursor.execute(sql, val)
+        mydb.commit()
     print("Registered", userName)
 
 
@@ -29,7 +49,7 @@ def show_users():
     for user in userList:
         print(user)
 
-#Functional, solves usecase 1?
+#Functional, solves use case 1?
 def login():
     valid_email=0
     valid_password=0
@@ -61,10 +81,11 @@ def check_email_login(email):
     checkUserEmail = mycursor.fetchone()
     if checkUserEmail is not None:
         print('Email exists')
-        return 1;
+        return 1
     else:
         print('Email does not exist! Try again')
     return 0
+#Functional
 def check_password(email,password):
     mycursor = mydb.cursor()
     sql = 'SELECT useremail FROM login WHERE useremail = %s AND Password = %s'
@@ -77,4 +98,25 @@ def check_password(email,password):
     else:
         print('Password is not valid! Try again')
     return 0
-login()
+def create_post(UserId):
+    print("Thread: 1")
+    print("Repy: 2")
+    print("Discussion Post: 3")
+    print("Exit any other input")
+    post_selection = input("Type of post: ")
+    if post_selection == 1:
+        post_type = "Thread"
+    elif post_selection == 2:
+        post_type = "Reply"
+    elif post_selection == 3:
+        post_type = "DiscussionPost"
+    else:
+        return
+    post_content = input("Content: ")
+    folder = input("Folder: ")
+    tag = input("Tag: ")
+create_user("Nick Fury", "FuryNicholasJ@gmail.com",1)
+
+
+
+
